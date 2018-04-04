10d9148c8b722d696acf245acd1f0809db15ceed
duyanh3110 <duyanh19962012@gmail.com>
2018-04-04 10:42:57 +0300

    Second commit

---
+++ data/burgerMenuData.js
@@ -0,0 +1,52 @@
+var BurgerMenuData = [
+    {
+      "key": "598a678278fee204ee51cd2c",
+      "name": "Bacon Burgers on Brioche Buns",
+      "imageUrl": "https://firebasestorage.googleapis.com/v0/b/snackers-9ff5b.appspot.com/o/images%2Fburger-menu%2Fburger-1.png?alt=media&token=6695ab88-5496-460b-a04f-925e219db409",
+      "foodDescription": "Ground prime rib, brisket, skirt steak",
+      "price": "8 €"
+    },
+    {
+      "key": "598a684f78fee204ee51cd2f",
+      "name": "Burger",
+      "imageUrl": "https://firebasestorage.googleapis.com/v0/b/snackers-9ff5b.appspot.com/o/images%2Fburger-menu%2Fburger-2.png?alt=media&token=e753c7ba-ef9a-4ad7-984e-f9e1ab64da57",
+      "foodDescription": "Bread, meat, chesse",
+      "price": "8.5 €"
+    },
+    {
+      "key": "598a687678fee204ee51cd30",
+      "name": "Bacon King",
+      "imageUrl": "https://firebasestorage.googleapis.com/v0/b/snackers-9ff5b.appspot.com/o/images%2Fburger-menu%2Fburger-3.png?alt=media&token=876db62f-2d0b-40d0-9776-c945add72d82",
+      "foodDescription": "Beef steak, bacon with bread",
+      "price": "7 €"
+    },
+    {
+      "key": "598a680178fee204ee51cd2e",
+      "name": "Big King",
+      "imageUrl": "https://firebasestorage.googleapis.com/v0/b/snackers-9ff5b.appspot.com/o/images%2Fburger-menu%2Fburger-4.png?alt=media&token=bd3c8156-49a4-4f55-af2c-1a3962b7d9c8",
+      "foodDescription": "Cheese, meat with cripsy onion",
+      "price": "9.5 €"
+    },
+    {
+      "key": "598a688878fee204ee51cd31",
+      "name": "Steakhouse",
+      "imageUrl": "https://firebasestorage.googleapis.com/v0/b/snackers-9ff5b.appspot.com/o/images%2Fburger-menu%2Fburger-5.png?alt=media&token=fc466be4-489c-4769-a310-d7eb797f3114",
+      "foodDescription": "Beef steak with cornflakes, onion",
+      "price": "5.5 €"
+    },
+    {
+      "key": "598a68b778fee204ee51cd32",
+      "name": "Chesse Burger",
+      "imageUrl": "https://firebasestorage.googleapis.com/v0/b/snackers-9ff5b.appspot.com/o/images%2Fburger-menu%2Fburger-6.png?alt=media&token=38cf52f4-34eb-4933-97f5-36fae39cab4b",
+      "foodDescription": "Beef steak with a cheese cake",
+      "price": "6.5 €"
+    },
+    {
+      "key": "598a67c478fee204ee51cd2d",
+      "name": "Texas Bacon King",
+      "imageUrl": "https://firebasestorage.googleapis.com/v0/b/snackers-9ff5b.appspot.com/o/images%2Fburger-menu%2Fburger-7.png?alt=media&token=2e6bbad9-8e4f-499d-9726-6b6a9ff1a87f",
+      "foodDescription": "Beef, bacon, cheese with O-rings",
+      "price": "10 €"
+    }
+];
+module.exports = BurgerMenuData;
--- screens/Burger.js
+++ screens/Burger.js
@@ -5,13 +5,159 @@ import {
   View,
   Text,
   StyleSheet,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+  FlatList,
 } from 'react-native';
+
+import { Rating } from 'react-native-elements';
+
+import BurgerMenuData from '../data/burgerMenuData';
+
+class FlatListItem extends Component {
+  render() {
+    let screenWidth = Dimensions.get('window').width;
+
+    return (
+        <View style={{
+          flex: 1,
+          flexDirection: 'row',
+          backgroundColor: 'white',
+          marginLeft: '5%',
+          marginRight: '5%',
+          marginBottom: '5%',
+          marginTop: '5%',
+          borderColor: '#f0f0f0',
+          borderWidth: 1,
+        }}>
+          <Image
+            style={{
+              width: screenWidth/2.5,
+              height: (screenWidth * 500 / 540)/2.5,
+            }}
+            source={{uri: this.props.item.imageUrl}}
+          />
+          <View style={{
+            flex: 1,
+            flexDirection: 'column',
+            marginLeft: '5%',
+            marginRight: '5%',
+          }}>
+            <Text style={{
+              fontSize: 16,
+              fontFamily: 'open-sans-Regular',
+              marginTop: '2%',
+              marginBottom: '2%',
+            }}>{this.props.item.name}</Text>
+            <Text style={{
+              fontSize: 12,
+              fontFamily: 'open-sans-Light',
+              marginBottom: '2%',
+            }}>{this.props.item.foodDescription}</Text>
+            <Text style={{
+              fontSize: 12,
+              fontFamily: 'open-sans-Light',
+            }}>
+              Price:
+              <Text> </Text>
+              <Text style={{
+                fontSize: 12,
+                fontFamily: 'open-sans-Regular',
+                marginBottom: '5%',
+              }}>
+                {this.props.item.price}
+              </Text>
+            </Text>
+
+            <View style={{
+              marginTop: '5%',
+              marginBottom: '5%',
+              flex: 1,
+              alignItems: 'flex-end',
+              flexDirection: 'row',
+              justifyContent:'space-between',
+            }}>
+              <Rating
+                type='custom'
+                ratingImage={require('../images/rating/star.png')}
+                ratingColor='white'
+                ratingCount={5}
+                startingValue={5}
+                imageSize={14}
+              />
+              <TouchableOpacity>
+                <Image
+                  style={{
+                    width: 14,
+                    height: 14,
+                  }}
+                  source={require('../images/rating/cart.png')}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+    );
+  }
+}
 
 export default class Burger extends Component {
   render() {
+    let screenWidth = Dimensions.get('window').width;
+
     return (
       <View style={styles.container}>
-        <Text>This is Burger Page</Text>
+        <Image
+          style={{width: screenWidth, height: screenWidth * 203 / 1080}}
+          source={require('../images/menu/cover.png')}
+        />
+
+        <View style={{flexDirection: 'row'}}>
+          <TouchableOpacity onPress={() => this.props.navigation.navigate('Menu')}>
+            <Image
+              style={{width: screenWidth/4, height: (screenWidth * 131 / 270)/4}}
+              source={require('../images/menu/menu.png')}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => this.props.navigation.navigate('Basket')}>
+            <Image
+              style={{width: screenWidth/4, height: (screenWidth * 131 / 270)/4}}
+              source={require('../images/menu/basket.png')}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => this.props.navigation.navigate('User')}>
+            <Image
+              style={{width: screenWidth/4, height: (screenWidth * 131 / 270)/4}}
+              source={require('../images/menu/user.png')}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => this.props.navigation.navigate('Feedback')}>
+            <Image
+              style={{width: screenWidth/4, height: (screenWidth * 131 / 270)/4}}
+              source={require('../images/menu/feedback.png')}
+            />
+          </TouchableOpacity>
+        </View>
+
+        <Image
+          style={{width: screenWidth, height: screenWidth * 176 / 1080}}
+          source={require('../images/burger/burger.png')}
+        />
+
+        <FlatList
+        data={BurgerMenuData}
+        renderItem={({item, index}) => {
+          return (
+            <TouchableOpacity
+              onPress={() => this.props.navigation.navigate('BurgerInfo')}
+            >
+              <FlatListItem item={item} index={index} />
+            </TouchableOpacity>
+          );
+        }}
+        />
+
       </View>
     );
   }
@@ -20,7 +166,5 @@ export default class Burger extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
   },
 });
