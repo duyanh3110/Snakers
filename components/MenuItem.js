f8d593b4f994f185f589a1b81114f35bf4572873
dothuan96 <dov.thuan96@gmail.com>
2018-04-11 13:54:06 +0300

    commit change flat list

--- components/MenuItem.js
+++ components/MenuItem.js
@@ -13,95 +13,9 @@ import { Rating } from 'react-native-elements';
 import { Actions } from 'react-native-router-flux';
 import Modal from 'react-native-modalbox';
 
+import FlatListItem from './FlatListItem';
 import MenuBar1 from './MenuBar1';
 import MenuBar2 from './MenuBar2';
-
-class FlatListItem extends Component {
-  render() {
-    let screenWidth = Dimensions.get('window').width;
-
-    return (
-        <View style={{
-          flex: 1,
-          flexDirection: 'row',
-          backgroundColor: 'white',
-          marginLeft: '5%',
-          marginRight: '5%',
-          marginBottom: '5%',
-          marginTop: '5%',
-          borderColor: '#f0f0f0',
-          borderWidth: 1,
-        }}>
-          <Image
-            style={{
-              width: screenWidth/2.5,
-              height: (screenWidth * 500 / 540)/2.5,
-            }}
-            source={{uri: this.props.item.imageUrl}}
-          />
-          <View style={{
-            flex: 1,
-            flexDirection: 'column',
-            marginLeft: '5%',
-            marginRight: '5%',
-          }}>
-            <Text style={{
-              fontSize: 16,
-              fontFamily: 'open-sans-Regular',
-              marginTop: '2%',
-              marginBottom: '2%',
-            }}>{this.props.item.name}</Text>
-            <Text style={{
-              fontSize: 12,
-              fontFamily: 'open-sans-Light',
-              marginBottom: '2%',
-            }}>{this.props.item.foodDescription}</Text>
-            <Text style={{
-              fontSize: 12,
-              fontFamily: 'open-sans-Light',
-            }}>
-              Price:
-              <Text> </Text>
-              <Text style={{
-                fontSize: 12,
-                fontFamily: 'open-sans-Regular',
-                marginBottom: '5%',
-              }}>
-                {this.props.item.price} + '$'
-              </Text>
-            </Text>
-
-            <View style={{
-              marginTop: '5%',
-              marginBottom: '5%',
-              flex: 1,
-              alignItems: 'flex-end',
-              flexDirection: 'row',
-              justifyContent:'space-between',
-            }}>
-              <Rating
-                type='custom'
-                ratingImage={require('../images/rating/star.png')}
-                ratingColor='white'
-                ratingCount={5}
-                startingValue={5}
-                imageSize={14}
-              />
-              <TouchableOpacity>
-                <Image
-                  style={{
-                    width: 14,
-                    height: 14,
-                  }}
-                  source={require('../images/rating/cart.png')}
-                />
-              </TouchableOpacity>
-            </View>
-          </View>
-        </View>
-    );
-  }
-}
 
 export default class MenuItem extends Component {
   constructor(props) {
--- data/homepageData.js
+++ data/homepageData.js
@@ -4,49 +4,49 @@ var homePageData = [
       "name": "Bacon Burgers on Brioche Buns",
       "imageUrl": "https://firebasestorage.googleapis.com/v0/b/snackers-9ff5b.appspot.com/o/images%2Fburger-menu%2FBacon%20Burgers.jpg?alt=media&token=06430ae5-f045-4be0-a5a9-c3f1986200e8",
       "foodDescription": "Ground prime rib, brisket, skirt steak",
-      "price": "8 €"
+      "price": 8
     },
     {
       "key": "5988278fed2c",
       "name": "Neapolitan",
       "imageUrl": "https://firebasestorage.googleapis.com/v0/b/snackers-9ff5b.appspot.com/o/images%2Fpizza-menu%2FNeapolitan.jpg?alt=media&token=465a3b51-b7f1-4d24-999b-036fcaf67e26",
       "foodDescription": "Tomato sauce, bufala mozzarella cheese",
-      "price": "8 €"
+      "price": 8
     },
     {
       "key": "5688878fe204ee51cd31",
       "name": "Blueberry Spinach Smoothie",
       "imageUrl": "https://firebasestorage.googleapis.com/v0/b/snackers-9ff5b.appspot.com/o/images%2Fdrink-menu%2F800_blueberry-spinach-breakfast-smoothie-460px_0.jpg?alt=media&token=c6fb2caf-2083-4d53-ab49-e04679fe4046",
       "foodDescription": "Blueberries and Spinach",
-      "price": "4 €"
+      "price": 4
     },
     {
       "key": "598a688878fecd31",
       "name": "Minetta Burger",
       "imageUrl": "https://firebasestorage.googleapis.com/v0/b/snackers-9ff5b.appspot.com/o/images%2Fburger-menu%2FMinetta.jpg?alt=media&token=76a74e34-a822-4b88-ac79-0c3c03e6aab2",
       "foodDescription": "Beef short rib and brisket",
-      "price": "5.5 €"
+      "price": 5.5
     },
     {
       "key": "598a68fee204ee51cd2e",
       "name": "Sicilian Pizza",
       "imageUrl": "https://firebasestorage.googleapis.com/v0/b/snackers-9ff5b.appspot.com/o/images%2Fpizza-menu%2FSicilian%20Pizza.jpg?alt=media&token=b265af84-8461-4868-a16b-e03edbf762e0",
       "foodDescription": "Onions, herbs, anchovies",
-      "price": "9.5 €"
+      "price": 9.5
     },
     {
       "key": "598a6782e204ee51cd2c",
       "name": "Chocolate-Mint Bars",
       "imageUrl": "https://firebasestorage.googleapis.com/v0/b/snackers-9ff5b.appspot.com/o/images%2Fdessert-menu%2FChocolate-Mint%20Bars.jpg?alt=media&token=70f49838-59cb-4663-b0e8-29040d3216ac",
       "foodDescription": "Dark chocolate chips and mint",
-      "price": "3 €"
+      "price": 3
     },
     {
       "key": "598a684f78fee204ef",
       "name": "Orange Juice",
       "imageUrl": "https://firebasestorage.googleapis.com/v0/b/snackers-9ff5b.appspot.com/o/images%2Fdrink-menu%2Forange%20juice.jpg?alt=media&token=fe9ce27f-5d87-417b-b404-4e3cb5f7c44c",
       "foodDescription": "Orange, sugar",
-      "price": "2 €"
+      "price": 2
     }
 ];
 module.exports = homePageData;
