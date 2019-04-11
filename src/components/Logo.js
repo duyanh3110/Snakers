0b3a73bbfce6957f87a95bd9f3c1aa7aa08859fa
MinhAnhS <35805776+MinhAnhS@users.noreply.github.com>
2019-04-11 13:02:42 +0300

    Update Logo.js

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -9,7 +9,9 @@ export default class Logo extends React.Component {
         <Image
           style={{width:screenWidth*0.7, height: (screenWidth*124/527)*0.7}}
           source={require('../images/logo2.png')}
+        
         />
+                  <div></div>
       </View>
     );
   }
